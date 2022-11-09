68a04e8e300fc1718963e62606fe311753500cfa
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-09 14:25:27 +0530

    Container_all_properties

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,37 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Text("Hii  flutter"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Myapp"),
+      ),
+      body: Center(
+        child: Container(
+          //  padding: const EdgeInsets.all(8),
+          alignment: Alignment.center,
+          height: 200,
+          width: 200,
+
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              // shape: BoxShape.circle,
+              color: Colors.red,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black,
+                  blurRadius: 5,
+                  offset: Offset(2.0, 5.0),
+                )
+              ],
+              gradient: LinearGradient(
+                  colors: [Colors.pink, Colors.yellow, Colors.blue])),
+          child: Text(
+            "I Im box",
+            style: TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
     );
   }
 }
