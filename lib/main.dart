c75d8cf15cecd10a5eff96eadd9f021db2b09c8a
NileshPathak <pathaknilesh1998@gmail.com>
2022-11-09 13:32:17 +0530

    main_file_create

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MaterialApp(
+    title: "myApplication",
+    home: Container(
+      color: Colors.white,
+    ),
+  ));
+} 
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
